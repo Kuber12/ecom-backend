079c909d4c82d2fbd2c4c8c074070eb904efe338
unknown <sherchananish11@gmail.com>
2024-04-09 21:49:25 +0545

    New updated Readme

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,29 @@
-# ecom-backend
+# E-commerce Backend (Node.js)
+
+E commerce Backend application.
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/Kuber12/ecom-backend.git
+```
+
+Go to the project directory
+
+```bash
+  cd backend
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run dev
+```
--- backend/config/dbConnection.ts
+++ backend/config/dbConnection.ts
@@ -1,18 +1,18 @@
-import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 dotenv.config();
 
-const connectionString = process.env.CONNECTION_STRING || '';
+const connectionString = process.env.CONNECTION_STRING || "";
 
 export async function connectToDatabase() {
   const client = new MongoClient(connectionString);
 
   try {
     await client.connect();
-    console.log('MongoDB Connected');
+    console.log("MongoDB Connected");
     return client;
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
     throw error;
   }
 }
